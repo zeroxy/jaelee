d0544cb896328843fba39912aef57d75a65e5ed8
김광진 <kjink.kim@samsung.com>
2015-12-11 13:07:32 +0900

    최적화

--- res/resource.js
+++ res/resource.js
@@ -1,8 +1,8 @@
-var wonjaelee1     = "wonjaelee1.png";
-var wonjaelee2     = "wonjaelee2.png";
-var wonjaelee3     = "wonjaelee3.png";
-var resources      = "resource.png";
-var garlic          = "garlic.png";
+var wonjaelee1     = "res/wonjaelee1.png";
+var wonjaelee2     = "res/wonjaelee2.png";
+var wonjaelee3     = "res/wonjaelee3.png";
+var resources      = "res/resource.png";
+var garlic         = "res/garlic.png";
 
 var g_resources = [
   wonjaelee1,
